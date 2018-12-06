e1dd7998eae1e7132a8a17a666b840800f2641f0
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2018-12-06 14:16:44 -0500

    add slopeY

---
+++ slopeY.m
@@ -0,0 +1,29 @@
+function DY = slopeY(t,Y)
+% central difference approximation for the slope of Y that handles unequal
+% spacing.
+
+%TODO: 
+% - add options for forward/backward differences, different orders, low-noise versions
+% - add options for different boundary strategies
+
+
+% centDiff=@(xp1,xm1) (xp1-xm1) / (hp1+h);
+
+DY=zeros(size(Y));
+
+%compute slope only at inner points
+for i=2:length(Y)-1
+    h=t(i)-t(i-1);
+    hp1=t(i+1)-t(i);
+    DY(i,:)=(Y(i+1,:)-Y(i-1,:))/(hp1+h);
+end
+
+%use difference that works at ends
+DY(1,:)=Y(2,:)-Y(1,:)/(t(2)-t(1));
+DY(end,:)=Y(end,:)-Y(end-1,:)/(t(end)-t(end-1));
+
+%clamp the endpoints to the second values
+% DY(1,:)=DY(2,:); 
+% DY(end,:)=DY(end-1,:);
+
+end
